79ac153024cea0335dafa0febc89ea5cb0a72a18
=Tara Petric <tara.petric@gmail.com>
2013-11-13 13:50:26 +0100

    database bug

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -56,6 +56,12 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 import dj_database_url
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default']= dj_database_url.config() 
 
