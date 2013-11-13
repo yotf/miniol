71afd44ab06d83fae311ccd1086cba398682dfc2
=Tara Petric <tara.petric@gmail.com>
2013-11-13 14:04:36 +0100

    test

---
+++ tt/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Test(models.Model):
+    att1 = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        retrn self.question_text
+
+    
