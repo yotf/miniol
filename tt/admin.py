3bc96fd5a9287b5b4156d1999b1c91b2571a3590
=Tara Petric <tara.petric@gmail.com>
2013-11-15 16:45:57 +0100

    admin bla

--- tt/admin.py
+++ tt/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from tt.models import Test,Aktivnost,ZavrseneAktivnosti,Student,Module
-admin.site.register(Test)
+from tt.models import Aktivnost,ZavrseneAktivnosti,Student,Module
+
 admin.site.register(Student)
 admin.site.register(Aktivnost)
 admin.site.register(ZavrseneAktivnosti)
