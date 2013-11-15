03524fed5ec7d42a372f542e25f2bf4cbc4a765e
=Tara Petric <tara.petric@gmail.com>
2013-11-15 16:30:12 +0100

    fix

--- tt/views.py
+++ tt/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from tt import Module
+from tt.models import Module
 
 
 def index(request):
-    module_list = Module.objects
+    module_list = Module.objects.all()
+    print type(Module.objects)
     context = {'module_list':module_list}
     return render(request,'tt/index.djhtml',context)
 
