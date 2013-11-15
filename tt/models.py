11618511d1b5a7a60b99b8108b5c1b86766aea4d
=Tara Petric <tara.petric@gmail.com>
2013-11-15 16:43:20 +0100

    activities

--- tt/models.py
+++ tt/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Test(models.Model):
-    att1 = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __unicode__(self):
-        return self.att1
 
 class Student(models.Model):
     user = models.OneToOneField(User)
@@ -20,17 +14,20 @@ class Student(models.Model):
     prezime = models.CharField(max_length=200)
     def __unicode__(self):
         return self.user
+        
+class Module(models.Model):
+    title = models.CharField(max_length=200,unique=True)
+
+    def __unicode__(self):
+        return self.title
 
 class Aktivnost(models.Model):
-    ime = models.CharField(max_length=200,unique=True)
+    module = models.ForeignKey(Module)
+    title = models.CharField(max_length=200,unique=True)
     is_active = models.BooleanField(default=True)
     def __unicode__(self):
         return self.ime
 
-class Module(models.Model):
-    title = models.CharField(max_length=200,unique=True)
-    def __unicode__(self):
-        return self.title
     
 class ZavrseneAktivnosti(models.Model):
     student = models.ForeignKey(Student)
